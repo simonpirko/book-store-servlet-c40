d69b54c7ea2de90ced3c34edc59ac549a4f6fd1a
Anton Novosad <noobito.2011@gmail.com>
2021-04-29 22:59:53 +0300

    in BookDa.java add methods getBookByData, getBookByGenre, getBookByGenreAndData in Book.java add field publicationDate

--- src/main/java/by/book/dao/BookDao.java
+++ src/main/java/by/book/dao/BookDao.java
@@ -3,12 +3,17 @@ package by.book.dao;
 import by.book.entity.Author;
 import by.book.entity.Book;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public interface BookDao {
     List<Book> getAll();
     Book getBookById(long id);
     Book getBookByName(String name);
+    List<Date> getBookByData(LocalDate date);
+    List<Book> getBookByGenre(String genre);
+    List<Book> getBookByGenreAndData(String genre, LocalDate date);
     List<Book> getBookByAuthor(Author author);
     void save(Book book);
     void delete(long id);
@@ -17,6 +22,7 @@ public interface BookDao {
     void updateAuthor(long id, Author author);
     void updatePrice(long id, int price);
     void updateGenre (long id, String genre);
+    void updatePublicationDate(long id, LocalDate date);
     boolean contains(String name, Author author);
 
 
--- src/main/java/by/book/entity/Book.java
+++ src/main/java/by/book/entity/Book.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -16,5 +17,6 @@ public class Book {
     private String description;
     private String genre;
     private List<Comment> comments;
+    private LocalDate publicationDate;
     private Author author;
 }
