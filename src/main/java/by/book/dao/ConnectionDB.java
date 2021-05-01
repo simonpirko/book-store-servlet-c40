e4050c77a8d637e602e1aef9245318e4d664fe31
tijani <viktorsee0@gmail.com>
2021-05-01 16:01:16 +0300

    Add class ConnectionDB for connecting to the database Add class Config for configuration for connection

---
+++ src/main/java/by/book/dao/ConnectionDB.java
@@ -0,0 +1,20 @@
+package by.book.dao;
+
+import by.book.config.Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    public static Connection getConnection() throws SQLException {
+        return  DriverManager.getConnection(
+                Config.getProperty(Config.DB_URL),
+                Config.getProperty(Config.DB_LOGIN),
+                Config.getProperty(Config.DB_PASSWORD));
+    }
+
+    public static void main(String[] args) throws SQLException {
+     Connection connection =   ConnectionDB.getConnection();
+    }
+}
