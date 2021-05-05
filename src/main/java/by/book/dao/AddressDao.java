0412c9ee320de32bf3863362f1201ec3a2df7c31
Anton Novosad <noobito.2011@gmail.com>
2021-05-05 18:03:30 +0300

    added method getById; change parameters in method contains

--- src/main/java/by/book/dao/AddressDao.java
+++ src/main/java/by/book/dao/AddressDao.java
@@ -3,17 +3,19 @@ package by.book.dao;
 import by.book.entity.Address;
 import by.book.entity.Store;
 import by.book.entity.User;
+import by.book.exception.DaoException;
 
 import java.util.List;
 
 public interface AddressDao {
     List<Address> getAll();
+    Address getById(long id) throws DaoException;
     Address getByUser(User user);
-    Address getStore(Store store);
-    void delete(long id);
+    Address getByStore(Store store);
+    void delete(long id)throws DaoException;
     void save(Address address);
-    void updateStreet(long id, String street);
-    void updateHome(long id, int home);
-    boolean contains(Address address);
+    void updateStreet(long id, String street)throws DaoException;
+    void updateHome(long id, int home)throws DaoException;
+    boolean contains(String street, int home);
 
 }
