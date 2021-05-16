0d32c30777c98e989da288a7ca585f8cf5ba7f22
IvanBylinovich <ib6020607@gmail.com>
2021-05-16 19:55:06 +0300

    added: ability to change the user's personal data, address and password. added servlets:ProfileEditServlet, ProfileEditAddressServlet, ProfileEditPasswordServlet, ProfileEditPersonalDateServlet. changed:   UserService - added 3 new method: changPassword, changAddress, changPersonalData.   editProfile.jsp - added warning message, which notifies you of the result of the change attempt                   -servlet addresses are registered

---
+++ src/main/java/by/book/web/servlet/user/ProfileEditPasswordServlet.java
@@ -0,0 +1,39 @@
+package by.book.web.servlet.user;
+
+import by.book.entity.User;
+import by.book.exception.UserDataException;
+import by.book.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/profile/editPassword", name = "ProfileEditPasswordServlet")
+public class ProfileEditPasswordServlet extends HttpServlet {
+
+   UserService userService = new UserService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String oldPassword = req.getParameter("oldPassword").trim();
+        String newPassword = req.getParameter("newPassword").trim();
+        String confirmNewPassword = req.getParameter("confirmNewPassword").trim();
+
+        User user =(User)req.getSession().getAttribute("user");
+
+        try {
+            userService.changPassword(user,oldPassword,newPassword, confirmNewPassword);
+            req.setAttribute("message", "Address changed successfully!");
+        } catch (UserDataException e) {
+            req.setAttribute("message", e.getMessage() );
+
+        }
+
+        req.getServletContext().getRequestDispatcher("/pages/user/editProfile.jsp").forward(req, resp);
+
+    }
+}
---
+++ src/main/java/by/book/web/servlet/user/ProfileEditServlet.java
@@ -0,0 +1,19 @@
+package by.book.web.servlet.user;
+
+import by.book.entity.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/profile/edit", name = "ProfileEditServlet")
+public class ProfileEditServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/pages/user/editProfile.jsp").forward(req, resp);
+    }
+
+}
