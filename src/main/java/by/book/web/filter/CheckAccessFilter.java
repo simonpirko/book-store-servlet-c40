daf9aa89e8a6cf49fde49cbed9af41848f115900
Zhelakovich_Vladimir <78911524+yourVovchik@users.noreply.github.com>
2021-05-17 14:04:26 +0300

    Added the ability to add books for the moder/admin

--- src/main/java/by/book/web/filter/CheckAccessFilter.java
+++ src/main/java/by/book/web/filter/CheckAccessFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(servletNames = { "StoreEdit", "StoreCreate", "StoreDelete", "StoreList", "BookEdit", "BookAdd", "BookManager", "BookRemove" })
+@WebFilter(servletNames = { "StoreEdit", "StoreCreate", "StoreDelete", "StoreList","AuthorAdd", "BookEdit", "BookAdd", "BookManager", "BookRemove" })
 public class CheckAccessFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
